fd223bc2eea5f8db20ded50874683829c1049ec9
“michael <354307169@qq.com>
2019-11-14 22:32:52 +0800

    init project

--- imooc-videos-dev-mini-api/src/main/java/com/imooc/Application.java
+++ imooc-videos-dev-mini-api/src/main/java/com/imooc/Application.java
@@ -3,6 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+
 @SpringBootApplication
 @ComponentScan(basePackages={"com.imooc"})
 public class Application {
